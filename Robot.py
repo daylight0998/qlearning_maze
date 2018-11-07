d8412d635d46de2b18ce02460291fcce7ab146b9
lixu <lixu2@vipkid.com.cn>
2018-11-08 01:31:25 +0800

    add files

--- Robot.py
+++ Robot.py
@@ -1,4 +1,5 @@
 import random
+import operator
 
 class Robot(object):
 
@@ -42,10 +43,10 @@ class Robot(object):
         """
         if self.testing:
             # TODO 1. No random choice when testing
-            pass
+            self.epsilon = 0
         else:
             # TODO 2. Update parameters when learning
-            pass
+            self.epsilon *= 0.95
 
         return self.epsilon
 
@@ -55,7 +56,7 @@ class Robot(object):
         """
 
         # TODO 3. Return robot's current state
-        return None
+        return self.maze.sense_robot()
 
     def create_Qtable_line(self, state):
         """
@@ -66,7 +67,7 @@ class Robot(object):
         # Qtable[state] ={'u':xx, 'd':xx, ...}
         # If Qtable[state] already exits, then do
         # not change it.
-        pass
+        self.Qtable.setdefault(state, {a: 0.0 for a in self.valid_actions})
 
     def choose_action(self):
         """
@@ -77,19 +78,21 @@ class Robot(object):
             # TODO 5. Return whether do random choice
             # hint: generate a random number, and compare
             # it with epsilon
-            pass
+            return random.uniform(0, 1.0) <= self.epsilon
 
         if self.learning:
             if is_random_exploration():
                 # TODO 6. Return random choose aciton
-                return None
+                return random.choice(self.valid_actions)
             else:
                 # TODO 7. Return action with highest q value
-                return None
+                return max(self.Qtable[self.state].items(), key=operator.itemgetter(1))[0]
         elif self.testing:
             # TODO 7. choose action with highest q value
+            return max(self.Qtable[self.state].items(), key=operator.itemgetter(1))[0]
         else:
             # TODO 6. Return random choose aciton
+            return random.choice(self.valid_actions)
 
     def update_Qtable(self, r, action, next_state):
         """
@@ -99,7 +102,7 @@ class Robot(object):
             pass
             # TODO 8. When learning, update the q table according
             # to the given rules
-
+            self.Qtable[self.state][action] = (1 - self.alpha) * self.Qtable[self.state][action] + self.alpha * ( r + self.gamma * max(self.Qtable[next_state].values()))
     def update(self):
         """
         Describle the procedure what to do when update the robot.
